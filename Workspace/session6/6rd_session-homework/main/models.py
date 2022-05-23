9d350565bd0012c76f8d19efcf4b853c1e1ccbc4
YoungWoong Park <assayw119@naver.com>
2022-05-23 21:14:11 +0900

    6주차 세션 과제

---
+++ Workspace/session6/6rd_session-homework/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    acti_title = models.CharField(max_length=200)
+    acti_date = models.CharField(max_length=100)
+    acti_insert = models.TextField()
+    acti_image = models.ImageField(upload_to = 'activities/', blank=True, null=True)
+
+    def __str__(self):
+        return self.acti_title
+
+
---
+++ Workspace/session6/6rd_session-homework/main/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include
+from .views import *
+
+app_name = 'main'
+urlpatterns = [
+    # introduce page URL 연결하기 with 별명사용
+    path('', showintroduce, name="showintroduce"),
+
+    # activities page URL 연결하기 with 별명사용
+    path('activities/', showactivities, name="showactivities"),
+
+    # favorite page URL 연결하기 with 별명사용
+    path('favorite/', showfavorite, name="showfavorite"),
+
+    # contact page URL 연결하기 with 별명사용
+    path('contact/', showcontact, name="showcontact"),
+
+    path('<int:id>', detail, name='detail'),
+
+    path('new/', new, name='new'),
+    
+    path('create/', create, name='create'),
+
+    path('edit/<int:id>', edit, name='edit'),
+
+    path('update/<int:id>', update, name='update'),
+
+    path('delete/<int:id>', delete, name='delete'),
+]
